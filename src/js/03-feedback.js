43945a634631d702c1dd7eed3b02ac75fac65241
antonaksyuk <87242472+antonaksyuk@users.noreply.github.com>
2021-12-03 10:57:19 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,38 @@
 import { throttle } from 'lodash';
 
-const isFeedbackFormFill = document.querySelector('.feedback-form')
-let textData;
+const form = document.querySelector('.feedback-form');
+const isFeedbackFormFill = 'feedback-form-state';
 
-isFeedbackFormFill.addEventListener('input', throttle(onFeedbackFormInput, 500));
-isFeedbackFormFill.addEventListener('submit', onFeedbackFormSubmit);
+form.addEventListener('input', throttle(onFeedbackFormInput, 500));
+form.addEventListener('submit', onFeedbackFormSubmit);
 
-function onFeedbackFormInput() {
-  const data = {
-    email: isFeedbackFormFill.email.value,
-    message: isFeedbackFormFill.message.value,
-  };
-  textData = data;
-  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+fillOutForm();
+function onFeedbackFormSubmit(event) {
+    event.preventDefault();
+    console.log({
+    email: form.email.value,
+    message: form.message.value,
+});
+    
+    event.currentTarget.reset();
+    localStorage.removeItem(isFeedbackFormFill);
 }
 
-function onFeedbackFormSubmit(evt) {
-  evt.preventDefault();
-  console.log(textData);
-  evt.currentTarget.reset();
-
-  localStorage.clear('feedback-form-state');
+function onFeedbackFormInput() {
+const formData = {
+    email: form.email.value,
+    message: form.message.value,
+};
+    
+localStorage.setItem(isFeedbackFormFill, JSON.stringify(formData));  
 }
 
 function fillOutForm() {
-  const filledForm = localStorage.getItem('feedback-form-state');
-  const parseForm = JSON.parse(filledForm);
-
-  if (filledForm) {
-    isFeedbackFormFill.email.value = parseForm.email;
-    isFeedbackFormFill.message.value = parseForm.message;
-  }
+    const filledForm = localStorage.getItem(isFeedbackFormFill);
+    const parsedForm = JSON.parse(filledForm);
+    
+    if (filledForm) {
+        form.email.value = parsedForm.email;
+        form.message.value = parsedForm.message;
+    } 
 }
-
-fillOutForm();
